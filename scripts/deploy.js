32d924c99359e348d773104fcf36ba9dae94bdbc
Sergii Bomko <1164492+aquiladev@users.noreply.github.com>
2022-09-12 18:46:50 +0100

    update deployment script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,26 +1,20 @@
+const { ShardedMerkleTree } = require("../src/merkle");
+
 // We require the Hardhat Runtime Environment explicitly here. This is optional
 // but useful for running the script in a standalone fashion through `node <script>`.
 //
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
-const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const Gtw = await ethers.getContractFactory("BABGateway");
+  const gtw = await Gtw.deploy();
+  await gtw.initialize();
+  await gtw.deployed();
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  tree = ShardedMerkleTree.fromFiles("providers/bab");
+  await gtw.updateRoot(tree.root);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
