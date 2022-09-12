71d166222e426883c0287f14028c7e4e2000f01f
Sergii Bomko <1164492+aquiladev@users.noreply.github.com>
2022-09-12 19:04:59 +0100

    fix warnings

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,26 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-import { useMetaMask } from 'metamask-react';
-import { utils } from 'ethers';
+import "./App.css";
+import { useMetaMask } from "metamask-react";
+import { utils } from "ethers";
 
-const AURORA_TESTNET_CHAINID = '0x4e454153';
-const CONTRACT_ADDRESS = '0x92687333b8a30d90f3203f1abf8621c7d28b647e';
+const AURORA_TESTNET_CHAINID = "0x4e454153";
+const CONTRACT_ADDRESS = "0x92687333b8a30d90f3203f1abf8621c7d28b647e";
 
-const ABI = 'function isRegistered(address account)';
+const ABI = "function isRegistered(address account)";
 
 async function verifyKyc(address, ethereum) {
   const iface = new utils.Interface([ABI]);
-  const callData = iface.encodeFunctionData('isRegistered', [address.toLowerCase()]);
+  const callData = iface.encodeFunctionData("isRegistered", [
+    address.toLowerCase(),
+  ]);
 
   const result = await ethereum.request({
-    method: 'eth_call',
-    params: [{
-      to: CONTRACT_ADDRESS,
-      data: callData
-    }],
+    method: "eth_call",
+    params: [
+      {
+        to: CONTRACT_ADDRESS,
+        data: callData,
+      },
+    ],
   });
 
-  if(result == '0x0000000000000000000000000000000000000000000000000000000000000001') {
+  if (
+    result ===
+    "0x0000000000000000000000000000000000000000000000000000000000000001"
+  ) {
     alert("You are KYC'd!");
   } else {
     alert("You are not KYC'd!");
@@ -28,31 +34,38 @@ async function verifyKyc(address, ethereum) {
 }
 
 function App() {
-  const { status, connect, account, chainId, ethereum, switchChain } = useMetaMask();
+  const { status, connect, account, chainId, ethereum, switchChain } =
+    useMetaMask();
 
-  if (status === "initializing") return <div>Synchronisation with MetaMask ongoing...</div>
+  if (status === "initializing")
+    return <div>Synchronisation with MetaMask ongoing...</div>;
 
-  if (status === "unavailable") return <div>MetaMask not available :(</div>
+  if (status === "unavailable") return <div>MetaMask not available :(</div>;
 
-  if (status === "notConnected") return <button onClick={connect}>Connect to MetaMask</button>
+  if (status === "notConnected")
+    return <button onClick={connect}>Connect to MetaMask</button>;
 
-  if (status === "connecting") return <div>Connecting...</div>
+  if (status === "connecting") return <div>Connecting...</div>;
 
   if (status === "connected") {
-    if(chainId !== AURORA_TESTNET_CHAINID)  {
+    if (chainId !== AURORA_TESTNET_CHAINID) {
       return (
         <div>
-          <button onClick={() => switchChain(AURORA_TESTNET_CHAINID)}>Change network</button>
+          <button onClick={() => switchChain(AURORA_TESTNET_CHAINID)}>
+            Change network
+          </button>
         </div>
       );
     }
 
     return (
       <div>
-        <div>Connected account {account} on chain ID {chainId}</div>
+        <div>
+          Connected account {account} on chain ID {chainId}
+        </div>
         <button onClick={() => verifyKyc(account, ethereum)}>Verify</button>
       </div>
-    )
+    );
   }
 
   return null;
